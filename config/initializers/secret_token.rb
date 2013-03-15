63e3e1fcf291da25d07783df1e42d1ed468b9bfe
Christina Chang <christina.chang@flatironschool.com>
2013-03-15 14:59:57 -0400

    add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'a0c02e67ef91080dbdf00a06c77f179791ba11602428143d835d4c742a668a2082b56cd07099077dc5c45f279926491379cedfa35dcf9832549271d6643fe170'
